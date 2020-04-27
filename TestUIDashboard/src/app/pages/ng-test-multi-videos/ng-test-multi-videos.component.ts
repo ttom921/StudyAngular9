db08727bb1fd09108dd850643075f4579ac4c273
ttom921 <ttom921@hotmail.com>
2020-04-27 12:15:33 +0800

    將測試移到多頻道頁面

--- TestUIDashboard/src/app/pages/ng-test-multi-videos/ng-test-multi-videos.component.ts
+++ TestUIDashboard/src/app/pages/ng-test-multi-videos/ng-test-multi-videos.component.ts
@@ -1,63 +1,98 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { CarVideoService } from 'src/app/_services/video/car-video.service';
 import { DomSanitizer } from '@angular/platform-browser';
 import { interval, fromEvent, combineLatest } from 'rxjs';
 import { MatVideoComponent } from 'src/app/_common/video/video.component';
+import { SyncVideoComponent } from 'src/app/_common/sync-video/sync-video.component';
+import { SyncVideoMgrService } from 'src/app/_common/sync-video/service/sync-video-mgr.service';
 
 @Component({
   selector: 'app-ng-test-multi-videos',
   templateUrl: './ng-test-multi-videos.component.html',
   styleUrls: ['./ng-test-multi-videos.component.scss']
 })
-export class NgTestMultiVideosComponent implements OnInit {
+export class NgTestMultiVideosComponent implements OnInit, AfterViewInit {
   @ViewChild('video1', { static: true }) video1: MatVideoComponent;
   @ViewChild('video2', { static: true }) video2: MatVideoComponent;
-  // @ViewChild('video3', { static: true }) video3: ElementRef;
-  // @ViewChild('video4', { static: true }) video4: ElementRef;
-  // @ViewChild('video5', { static: true }) video5: ElementRef;
-  // @ViewChild('video6', { static: true }) video6: ElementRef;
-  // @ViewChild('video7', { static: true }) video7: ElementRef;
-  // @ViewChild('video8', { static: true }) video8: ElementRef;
-  // srcs = [];
+  @ViewChild('video3', { static: true }) video3: MatVideoComponent;
+  @ViewChild('video4', { static: true }) video4: MatVideoComponent;
+  @ViewChild('video5', { static: true }) video5: MatVideoComponent;
+  @ViewChild('video6', { static: true }) video6: MatVideoComponent;
+  @ViewChild('video7', { static: true }) video7: MatVideoComponent;
+  @ViewChild('video8', { static: true }) video8: MatVideoComponent;
+
+  @ViewChild('syncvideo', { static: true }) syncvideo: SyncVideoComponent;
   constructor(
     private carVideoService: CarVideoService,
-    private sanitizer: DomSanitizer
+    private sanitizer: DomSanitizer,
+    private syncVideoMgrService: SyncVideoMgrService,
   ) { }
+  ngAfterViewInit(): void {
 
+
+    this.syncVideoMgrService.initRxJSevent();
+
+    //測試
+    // this.syncVideoMgrService.canPlay$.subscribe(data => {
+    //   console.log(`syncVideoMgrService.canPlay$=${data}`);
+    //   this.syncVideoMgrService.play();
+    // });
+    // this.syncVideoMgrService.waiting$.subscribe(data => {
+    //   console.log(`syncVideoMgrService.waiting$=${data}`);
+    //   this.syncVideoMgrService.pause();
+    // });
+  }
   ngOnInit(): void {
-    //console.dir(this.video1);
+    this.syncvideo.addVideo(this.video1);
+    this.syncvideo.addVideo(this.video2);
+    this.syncvideo.addVideo(this.video3);
+    this.syncvideo.addVideo(this.video4);
+    //
+    this.syncvideo.addVideo(this.video5);
+    this.syncvideo.addVideo(this.video6);
+    this.syncvideo.addVideo(this.video7);
+    this.syncvideo.addVideo(this.video8);
 
-    const v1$ = fromEvent(this.video1.getVideoTag(), 'canplay');
-    const v2$ = fromEvent(this.video2.getVideoTag(), 'canplay');
-    // v1$.subscribe(data => {
-    //   console.dir(data);
-    //   console.log(`canplay$`);
+    this.syncVideoMgrService.setMainVideo(this.video1);
+    this.loadVideo();
+
+
+
+    // console.dir(this.video1.getVideoTag());
+    // this.video1.getVideoTag().addEventListener('canplay', function (event) {
+    //   console.log(`canplay=${event}`);
+    //   console.dir(event);
+    // });
+    // this.video1.getVideoTag().addEventListener('waiting', function (event) {
+    //   console.log(`waiting=${event}`);
+    //   console.dir(event);
     // });
 
-    const allEvents$ = combineLatest(
-      v1$,
-      v2$,
-    );
-    allEvents$.subscribe(data => {
-      console.dir(data);
-      console.log(`allEvents canPlay=${data}`);
-      // this.canPlay$.next(true);
-      // this.waiting$.next(false);
-    });
 
-    this.loadVideo();
 
   }
   loadVideo() {
-    let vdapi;
+    this.testcarvieo();
+    //this.testcaronlinevideo();
+    //let vdapi;
+    //let video=this.video1.getVideoTag();
+
     //vdapi = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4';
 
     //vdapi = "http://localhost:4200/assets/[DATE(2016-08-14)TIME(14-00-00)]CH01.mp4";
-    vdapi = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4';
-    this.video1.src = vdapi;
-    //vdapi = "https://nkoehler.github.io/mat-video/assets/NASA.mp4"
-    // //vdapi = "http://localhost:4200/assets/[DATE(2016-08-14)TIME(14-00-00)]CH01.mp4";
-    this.video2.src = vdapi;
+    // vdapi = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4';
+    // //console.log(this.video1);
+    // this.video1.src = vdapi;
+    // //this.video1.setVideoSrc(vdapi);
+
+
+
+    // //this.cd.detectChanges();
+    // //console.log(this.video1);
+    // //this.video1.getVideoTag().src = vdapi;
+    // // //vdapi = "https://nkoehler.github.io/mat-video/assets/NASA.mp4"
+    // // //vdapi = "http://localhost:4200/assets/[DATE(2016-08-14)TIME(14-00-00)]CH01.mp4";
+    // this.video2.src = vdapi;
     // //vdapi = "http://localhost:4200/assets/[DATE(2016-08-14)TIME(14-00-00)]CH01.mp4";
     // this.video3.src = vdapi;
     // //vdapi = "http://localhost:4200/assets/[DATE(2016-08-14)TIME(14-00-00)]CH01.mp4";
@@ -80,5 +115,39 @@ export class NgTestMultiVideosComponent implements OnInit {
 
 
 
+  }
+  testcarvieo() {
+    let vdapi;
+    vdapi = "http://localhost:4200/assets/[DATE(2016-08-14)TIME(14-00-00)]CH01.mp4";
+    this.video1.src = vdapi;
+    this.video2.src = vdapi;
+    vdapi = "http://localhost:4200/assets/[DATE(2016-08-14)TIME(14-00-00)]CH05.mp4";
+    this.video3.src = vdapi;
+    this.video4.src = vdapi;
+    //
+    //
+    vdapi = "http://localhost:4200/assets/[DATE(2016-08-14)TIME(14-00-00)]CH05.mp4";
+
+    this.video5.src = vdapi;
+    this.video6.src = vdapi;
+    vdapi = "http://localhost:4200/assets/[DATE(2016-08-14)TIME(14-00-00)]CH01.mp4";
+    this.video7.src = vdapi;
+    this.video8.src = vdapi;
+  }
+  testcaronlinevideo() {
+    let vdapi;
+    vdapi = "http://ttom.tplinkdns.com:4200/assets/[DATE(2016-08-14)TIME(14-00-00)]CH01.mp4";
+    this.video1.src = vdapi;
+    this.video2.src = vdapi;
+    vdapi = "http://ttom.tplinkdns.com:4200/assets/[DATE(2016-08-14)TIME(14-00-00)]CH05.mp4";
+    this.video3.src = vdapi;
+    this.video4.src = vdapi;
+    //
+    vdapi = "http://ttom.tplinkdns.com:4200/assets/[DATE(2016-08-14)TIME(14-00-00)]CH01.mp4";
+    this.video5.src = vdapi;
+    this.video6.src = vdapi;
+    vdapi = "http://ttom.tplinkdns.com:4200/assets/[DATE(2016-08-14)TIME(14-00-00)]CH05.mp4";
+    this.video7.src = vdapi;
+    this.video8.src = vdapi;
   }
 }
