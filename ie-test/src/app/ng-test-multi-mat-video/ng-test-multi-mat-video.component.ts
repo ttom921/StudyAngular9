a976a9f8f4e68730d8087e8229d25acdd1d20ed6
ttom921 <ttom921@hotmail.com>
2020-06-08 17:09:37 +0800

    加入測試單一影像專案

--- ie-test/src/app/ng-test-multi-mat-video/ng-test-multi-mat-video.component.ts
+++ ie-test/src/app/ng-test-multi-mat-video/ng-test-multi-mat-video.component.ts
@@ -8,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class NgTestMultiMatVideoComponent implements OnInit {
   //videosrc = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4";
   //720
-  videosrc = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4";
+  //videosrc = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4";
 
   //videosrc = "http://localhost:4200/assets/[DATE(2020-05-15)TIME(09-22-00)]CH06_aac.mp4";
-  //videosrc = "http://localhost:4200/assets/test.mp4";
+  videosrc = "http://localhost:4200/assets/test.mp4";
   constructor() { }
 
   ngOnInit(): void {
